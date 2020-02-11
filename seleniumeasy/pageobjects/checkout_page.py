a47a37975fd85958d8efbbcfd80566f18104b868
Dmitri <dinevich.python@gmail.com>
2020-02-11 14:26:27 -0800

    New Commit

--- seleniumeasy/pageobjects/checkout_page.py
+++ seleniumeasy/pageobjects/checkout_page.py
@@ -7,9 +7,13 @@ class CheckoutPage:
 
 	card_title = (By.CSS_SELECTOR, '.card-title a')
 	card_footer = (By.CSS_SELECTOR, '.card-footer button')
+	checkout = (By.XPATH, '//a[@class="nav-link btn btn-primary"]')
 
 	def get_card_titles(self):
 		return self.browser.find_elements(*CheckoutPage.card_title)
 
 	def get_card_footers(self):
 		return self.browser.find_elements(*CheckoutPage.card_footer)
+
+	def get_checkout_items(self):
+		return self.browser.find_element(*CheckoutPage.checkout)
